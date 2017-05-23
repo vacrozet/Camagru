264ea688a3d584f77ec00db64996c929d698542c
vacrozet <vacrozet@student.42.fr>
2017-05-23 22:44:49 +0200

    --wip-- [skip ci]

--- camagru_ok/vue/header.php
+++ camagru_ok/vue/header.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 $_SESSION['user_name'] = "vacrozet";
+$_SESSION['admin'] = "OUI";
 ?>
 <!DOCTYPE html>
 <html>
@@ -13,48 +14,24 @@ $_SESSION['user_name'] = "vacrozet";
 <body>
 	<header class="header">
 		<div class="titre"><p id="titre"><I>Camagru</I></p></div>
-		<?php if ($_SESSION['user_name'] != "") { ?>
 		<div style="margin-right: 10px;">
+		<?php if ($_SESSION['user_name'] == "") { ?>
 			<form method="post" action="./script/connexion.php">
-				<input type="Login" name="Login" placeholder="Login">
-				<input type="Password" name="Passwd" placeholder="Password">
+				<input class="input_connect" type="Login" name="Login" placeholder="Login">
+				<input class="input_connect" type="Password" name="Passwd" placeholder="Password">
 				<button type="submit" name="Connexion" value="Sign On">Sign On</button>
 			</form>
 		</div>
-		<?php } else { ?>
-
+		<?php } else 
+		{ 
+			if ($_SESSION['admin'] == "OUI")
+			{
+				echo "<img class=\"img\" src=\"./img/server.png\">";
+			 } ?>
+		<a href=""><img class="img" src="./img/logout.png"></a>
+		<a href=""><img class="img" src="./img/instagram.png"></a>
+		<a href=""><img src="./img/user-account-box.png" href=""></a>
+		</div>
 		<?php } ?>
 
 	</header>
-<!-- 	<header class="header">
-		<div class="inheaderborder">
-		<div class="enfant sizeborderleft" style="align-items: center;">
-  				<div style="height: 100%; margin-left: 10px;">
-	 				<a style="height: 100%;" href="../index.php"><img style="" src="../img/web-page-home.png" title=""></a>
-	 			</div>
-	 			<div style="height: 100%;">
-	 				<a style="margin-left: 10px;" href="../script/logout.php"><img src="../img/logout.png"></a>
-		 		</div>
-	 			<?php
-	 			// if ($_SESSION['admin'] == "OUI")
-	 			{
-	 			?>
-	 				<div style="height: 100%;">
-		 				<a style="margin-left: 10px;" href="./script/admin.php"><img src="../img/database.png"></a>
-			 		</div>
-	 			<?php	
-	 			}
-	 			?>
-	 		</div>
- 		</div>
-		<div class="inheadercenter">
-			<div class="enfant"><U><I><h1>Camagru</h1></I></U></div>
-		</div>
-		<div class="inheaderborder">
-			<div class="enfant">
-					<a href="#">
-					<img  style="max-height: 90px; max-width: 90px;" class="appareil" src="../img/insta.png">
-				</a>
-			</div>
-		</div>		
-	</header> -->
