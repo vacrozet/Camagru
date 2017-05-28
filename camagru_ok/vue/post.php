2a86b410ee61f8778c39abed7384bccbbb97e57f
vacrozet <vacrozet@student.42.fr>
2017-05-28 17:55:04 +0200

    --wip-- [skip ci]

--- camagru_ok/vue/post.php
+++ camagru_ok/vue/post.php
@@ -13,11 +13,11 @@
 		</div>
 		<div id=command>
 			<button style="background-color: transparent; border: none;"><img id=capture src="./img/capture.png"></button>
-				<input type="hidden" id="inputimg_pris" name="img_pris">
-				<input type="hidden" id="inputpng_pris" name="png_pris">
-				<input type="hidden" id="inputtop_pris" name="top_pris">
-				<input type="hidden" id="inputleft_pris" name="left_pris">
-			<form method="POST" action="img_merge_2.php">
+			<input type="hidden" id="inputimg_pris" name="img_pris">
+			<input type="hidden" id="inputpng_pris" name="png_pris">
+			<input type="hidden" id="inputtop_pris" name="top_pris">
+			<input type="hidden" id="inputleft_pris" name="left_pris">
+			<form method="POST" action="img_merge.php">
 				<input type="hidden" id="inputimg_prec" name="img_prec">
 				<input type="hidden" id="inputpng_prec" name="png_prec">
 				<input type="hidden" id="inputtop_prec" name="png_prec">
@@ -27,7 +27,11 @@
 			<button style="background-color: transparent; border: none;"><img style="cursor: pointer;`" onclick="deleteElemCapt()" src="./img/recycle.png"></button>
 			<button style="background-color: transparent; border: none;"><img src="./img/upload.png"></button>
 		</div>
- 		<div id=photo_prec>
+<!-- 		<div id=upload>
+			<h2>Upload an image</h2>
+			<input type="file" name="fichier" onchange="file_upload()">
+		</div>
+ --> 		<div id=photo_prec>
  		</div>
 	</div>
 	<div id=defile_png>
